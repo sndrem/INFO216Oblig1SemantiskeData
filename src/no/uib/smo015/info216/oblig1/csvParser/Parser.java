aa470af4b2f8a050a17d83d86c6faaa65789b6c0
Sindre Moldeklev <sndrem@live.no>
2015-04-16 13:06:36 +0200

    Mye nytt

--- src/no/uib/smo015/info216/oblig1/csvParser/Parser.java
+++ src/no/uib/smo015/info216/oblig1/csvParser/Parser.java
@@ -9,12 +9,15 @@ import java.text.NumberFormat;
 
 import no.uib.smo015.info216.HappyOntology.HappyOnt;
 import no.uib.smo015.info216.Utils.StringUtilities;
-import no.uib.smo015.info216.oblig1.model.DataModel;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.ontology.DatatypeProperty;
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.vocabulary.RDF;
 import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.vocabulary.XSD;
 
 /**
  * 
@@ -39,12 +42,12 @@ public class Parser {
 	 * @return a Model object for Jena to work with
 	 * 
 	 */
-	public Model readFile(DataModel data, String fileToRead) {
+	public OntModel readFile(OntClass ontClass, OntModel owlModel, String fileToRead) {
 		reader = null;
 		String line = "";
 		String split = ";";
 
-		Model tempModel = data.getHpiModel();
+//		Model tempModel = data.getHpiModel();
 
 		try {
 			File fileToParse = new File(fileToRead);
@@ -58,8 +61,91 @@ public class Parser {
 				// Valgt 1 fordi det er kolonne 2 i csv-filen som inneholder countryNames
 				String countryName = compact(props[1]);
 				
+				//Opprett datatype properties
 				NumberFormat nf = NumberFormat.getInstance();
-				Resource res = tempModel.createResource(HappyOnt.NS + countryName);
+				Individual countryIndividual = ontClass.createIndividual(HappyOnt.NS + countryName);
+					
+				DatatypeProperty rankProp = owlModel.createDatatypeProperty(HappyOnt.NS + "rank");
+								rankProp.addDomain(ontClass);
+								rankProp.addRange(XSD.integer);
+				
+				DatatypeProperty subRegionProp = owlModel.createDatatypeProperty(HappyOnt.NS + "subRegion");
+								subRegionProp.addDomain(ontClass);
+								subRegionProp.addRange(XSD.xstring);
+								
+				DatatypeProperty lifeExpectancyProp = owlModel.createDatatypeProperty(HappyOnt.NS + "lifeExpectancy");
+								lifeExpectancyProp.addDomain(ontClass);
+								lifeExpectancyProp.addRange(XSD.xfloat);
+				
+				DatatypeProperty wellBeingProp = owlModel.createDatatypeProperty(HappyOnt.NS + "wellBeing");
+								wellBeingProp.addDomain(ontClass);
+								wellBeingProp.addRange(XSD.xfloat);
+				
+				DatatypeProperty happyLifeYearsProp = owlModel.createDatatypeProperty(HappyOnt.NS + "happyLifeYears");
+								happyLifeYearsProp.addDomain(ontClass);
+								happyLifeYearsProp.addRange(XSD.xfloat);
+								
+				DatatypeProperty footPrintProp = owlModel.createDatatypeProperty(HappyOnt.NS + "footPrint");
+								footPrintProp.addDomain(ontClass);
+								footPrintProp.addRange(XSD.xfloat);
+				
+				DatatypeProperty happyIndexProp = owlModel.createDatatypeProperty(HappyOnt.NS + "happyIndex");
+								happyIndexProp.addDomain(ontClass);
+								happyIndexProp.addRange(XSD.xfloat);
+								
+				DatatypeProperty populationProp = owlModel.createDatatypeProperty(HappyOnt.NS + "population");
+								populationProp.addDomain(ontClass);
+								populationProp.addRange(XSD.integer);
+								
+				DatatypeProperty gdpProp = owlModel.createDatatypeProperty(HappyOnt.NS + "gdp");
+								gdpProp.addDomain(ontClass);
+								gdpProp.addRange(XSD.integer);
+								
+				DatatypeProperty regionProp = owlModel.createDatatypeProperty(HappyOnt.NS + "region");
+								regionProp.addDomain(ontClass);
+								regionProp.addRange(XSD.xstring);
+				
+				DatatypeProperty descriptionProp = owlModel.createDatatypeProperty(HappyOnt.NS + "description");
+								descriptionProp.addDomain(ontClass);
+								descriptionProp.addRange(XSD.xstring);
+								
+				
+				// Opprett literaler
+				Literal rankLit = owlModel.createTypedLiteral(props[0]);
+				Literal subRegionLit = owlModel.createTypedLiteral(props[2]);
+				Literal lifeExpectLit = owlModel.createTypedLiteral(props[3]);
+				Literal wellBeingLit = owlModel.createTypedLiteral(props[4]);
+				Literal happyLifeYearLit = owlModel.createTypedLiteral(props[5]);
+				Literal footPrintLit = owlModel.createTypedLiteral(props[6]);
+				Literal happyIndexLit = owlModel.createTypedLiteral(props[7]);
+				Literal populationLit = owlModel.createTypedLiteral(props[8]);
+				Literal gdpLit = owlModel.createTypedLiteral(props[9]);
+				Literal regionLit = owlModel.createTypedLiteral(computeRegion(props[2]));
+				Literal descriptionLit = owlModel.createTypedLiteral(StringUtilities.congoConvert(StringUtilities.underscoreRemoval(countryName)) + " is a country in the region of the " + 
+															StringUtilities.underscoreRemoval(computeRegion(props[2])) + "." + "\nThe population is ca. " + nf.format(new Integer(props[8])) + 
+						                                    " and the Happy Index was measured to " + props[7] + " in 2012.\n" +
+						                                    "The well being is " + props[4] + " and they have a measured\n" +
+						                                    "happy years of " + props[5] + ". They are ranked as number " + 
+						                                    props[0] + " of all the countries.\nThis is based " +
+						                                    "on their geologial footprint which is " + props[6] + "\nand their " +
+						                                    "well being of " + props[4]);
+				
+				Literal labelLit = owlModel.createTypedLiteral(StringUtilities.congoConvert(StringUtilities.underscoreRemoval(countryName)));
+								
+				
+				
+				
+				
+		
+				
+				
+				
+				
+				
+				
+				
+				
+						tempModel.createResource(HappyOnt.NS + countryName);
 						res.addProperty(data.getId(),tempModel.createTypedLiteral(index))
 						.addLiteral(data.getRank(), new Integer(props[0]))
 						.addLiteral(data.getSubRegion(), props[2])
@@ -113,8 +199,6 @@ public class Parser {
 
 		return tempModel;
 	}
-	
-	
 
 	private String computeRegion(String subRegion) {
 		if(subRegion.substring(0, 1).equalsIgnoreCase("1")){
